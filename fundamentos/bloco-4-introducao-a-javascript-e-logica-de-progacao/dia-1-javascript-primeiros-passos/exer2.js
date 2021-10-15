03d0dc61877d6fded9c838a9f5aa11f824cd705a
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2021-10-15 14:49:00 -0300

    exercicios 1 e 2.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-progacao/dia-1-javascript-primeiros-passos/exer2.js
@@ -0,0 +1,15 @@
+const n1 = 25;
+const n2 = 58;
+
+let maiorNum;
+
+if(n1>n2){
+    maiorNum = n1;
+} else {
+    maiorNum = n2;
+}
+console.log(maiorNum);
+    
+//return maiorNum;//
+//não consegui fazer o "retorno do resultado"//
+
