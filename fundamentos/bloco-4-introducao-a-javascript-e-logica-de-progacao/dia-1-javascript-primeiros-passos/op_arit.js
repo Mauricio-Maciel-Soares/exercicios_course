af994ac539c30d0b90b22697a6246e47a8a9011b
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2021-10-15 14:06:10 -0300

    todos os programas em um arquivo

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-progacao/dia-1-javascript-primeiros-passos/op_arit.js
@@ -0,0 +1,34 @@
+const a = 2;
+const b = 7;
+
+let subtracao;
+
+subtracao = a - b;
+if(subtracao < 0){
+    subtracao = subtracao * (-1);
+}
+    console.log('O resultado da subtração é igual a',subtracao,'.');
+
+
+let soma;
+soma = a + b;
+
+    console.log('O resultado da soma é igual a',soma,'.');
+
+let multiplicacao;
+multiplicacao = a * b;
+
+    console.log('O resultado da multiplicação é igual a',multiplicacao,'.');
+
+    
+    let divisao;
+    divisao = a / b;
+    
+        console.log('O resultado da divisão é igual a',divisao,'.');
+    //pesquisar como limitar casas decimais//
+
+
+    let modulo;
+    modulo = a % b;
+
+    console.log('O resultado da operação é igual a',modulo,'.');
