bb95839adafc97193b96080eca614bd3811a92e1
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2021-10-18 07:45:43 -0300

    criacao de variavel/resolucao

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-progacao/dia-1-javascript-primeiros-passos/exer5.js
@@ -0,0 +1,12 @@
+const ang1 = 60;
+const ang2 = 40;
+const ang3 = 60;
+
+let triangle;
+triangle = ang1 + ang2 + ang3;
+
+if(triangle == 180){
+    console.log('true');
+} else {
+    console.log('false');
+}
