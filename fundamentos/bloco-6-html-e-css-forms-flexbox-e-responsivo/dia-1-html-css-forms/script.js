bc07a4f02b58d5972d97038f24d84a21d97b4d04
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2021-11-22 15:01:32 -0300

    6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -3,9 +3,16 @@ const fieldset = document.querySelector("#fieldset-1");
 // create prevent default button;
 const preventButton = document.querySelector(".preventButton");
 const button = document.createElement("button");
+button.type = 'submit';
 button.id = "preButton";
-button.innerText = "Prevent Default";
+button.innerText = "Submeter";
 preventButton.appendChild(button);
+
+// create clear button;
+const clearButton = document.createElement("button");
+clearButton.id = "clearButton";
+clearButton.innerText = "Limpar";
+preventButton.appendChild(clearButton);
 
 // creat options to input state;
 const optStates = document.querySelector("#state");
@@ -69,8 +76,6 @@ let stateSimbol = [
     'TO'
 ];
 
-
-
 for (let i = 0; i < stateList.length; i += 1) {
   let state = stateList[i];
   let option = document.createElement("option");
@@ -82,8 +87,38 @@ for (let i = 0; i < stateList.length; i += 1) {
 for (let i = 0; i < stateSimbol.length; i += 1) {
     const simbol = stateSimbol[i];
     const option = document.querySelector('.options');
-
     option.value = simbol;
-
     optStates.appendChild(option);
 }
+
+const visual = document.querySelector('#visual');
+const email = document.querySelector('#email');
+const fullName = document.querySelector('#full-name');
+const cpf = document.querySelector('#cpf');
+const address = document.querySelector('#address');
+const city = document.querySelector('#city');
+const state = document.querySelector('#state');
+const placeType = document.querySelector('#place-type');
+const curriculo = document.querySelector('#curriculo');
+const cargo = document.querySelector('#cargo');
+const descargo = document.querySelector('#descargo');
+const dataInicio = document.querySelector('#data-inicio');
+
+
+function getVisual (event) {
+    event.preventDefault();
+    visual.innerHTML = email.value;
+    visual.innerHTML = fullName.value;
+    visual.innerHTML = cpf.value;
+    visual.innerHTML = address.value;
+    visual.innerHTML = city.value;
+    visual.innerHTML = state.value;
+    visual.innerHTML = placeType.value;
+    visual.innerHTML = curriculo.value;
+    visual.innerHTML = cargo.value;
+    visual.innerHTML = descargo.value;
+    visual.innerHTML = dataInicio.value;
+}
+
+const defaultButton = document.querySelector('#preButton');
+defaultButton.addEventListener('click', getVisual);
